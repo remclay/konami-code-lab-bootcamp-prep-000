b0f2f8837c4ed7f6ac0a4bbecd8cffacde6f76b2
Remmy Clay <remmy.lauren@gmail.com>
2017-10-31 06:09:40 +0000

    automatically backed up by learn

--- konami_code.js
+++ konami_code.js
@@ -2,7 +2,7 @@ const code = [38, 38, 40, 40, 37, 39, 37, 39, 66, 65]
 
 function init() {
   var i = 0;
-  document.body.addEventListener('keydown', function keydownTracker(e) {
+  document.body.addEventListener('keydown', function(e) {
     const keyValue = parseInt(e.which || e.detail);
     if (key === code[i]) {
       i++;
