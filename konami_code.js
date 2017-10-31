2534511ee7017b0027f6dc1912d014ac84e6c9bd
Remmy Clay <remmy.lauren@gmail.com>
2017-10-31 05:48:49 +0000

    automatically backed up by learn

--- konami_code.js
+++ konami_code.js
@@ -4,7 +4,7 @@ function init() {
   var body = document.getElementsByTagName('body');
   let i = 0;
 
-  body.addEventListener('keydown', keydownTracker);
+  document.addEventListener('keydown', keydownTracker);
   function keydownTracker(e){
     const keyValue = parseInt(e.which || e.detail);
     debugger
