4ce44cc43c85bd6e1ed9e85b5c0c4974e7067909
Remmy Clay <remmy.lauren@gmail.com>
2017-10-31 05:02:13 +0000

    automatically backed up by learn

--- konami_code.js
+++ konami_code.js
@@ -1,6 +1,22 @@
 const code = [38, 38, 40, 40, 37, 39, 37, 39, 66, 65]
 
 function init() {
-  // Write your JavaScript code inside the init() function
+  var body = document.getElementsByTagName('body');
+  let i = 0;
 
+  body.addEventListener("keydown", keydownTracker);
+  function keydownTracker(){
+
+    const key = parstInt(e.detail || e.which);
+    console.log((e.detail) 'eD')
+    if (key === code[i]) {
+      i++;
+      if (i === code.length) {
+        alert("Congratulations!");
+        index = 0;
+      }
+    } else {
+      index = 0;
+    }
+  }
 }
